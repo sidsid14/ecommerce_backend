5e9e959db04843820e0a0ef87782e79b0e7c4e1c
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-04-09 11:17:15 +0530

    First Commit

---
+++ src/test/java/com/st/commerce/SprintBootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.st.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
